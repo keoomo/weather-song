d9da50c969ff7f0ed6ea62338602571b4bf232e3
joy lin <161982728+keoomo@users.noreply.github.com>
2026-03-13 01:05:15 -0400

    Refactor weather fetching and drawing functions

--- script.js
+++ script.js
@@ -1,62 +1,50 @@
 //define variable for our location
 let locationField;
-
-let audioCtx;
-
-let freq = 0;
-
 let infoField;
+let canvas;
+let ctx;
 
 //wait until html document is loaded so that we can access the keyboard input field
 document.addEventListener('DOMContentLoaded', function(event) { 
   //locationField = document.getElementById("location");
   locationField = document.querySelector("#location");
   infoField = document.getElementById('info');
-})
+  canvas = document.getElementById('landscape');
+  ctx = canvas.getContext('2d');
 
-// create web audio api context
-audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-
-// create Oscillator node
-const oscillator = audioCtx.createOscillator();
-
-oscillator.type = "square";
-oscillator.frequency.setValueAtTime(freq, audioCtx.currentTime); // value in hertz
-oscillator.connect(audioCtx.destination);
-oscillator.start();
+});
 
 function sonify(){
-  console.log(locationField.value);
-
-
-
-  
 fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'+locationField.value +'?key=YH5QF5SWJL5LZ8SFPZFT2RCC2')
-.then(response => response.json())
+	.then(response => response.json())
 	.then(response => {
-    console.log(response);
-    console.log("the weather over the next few days is:" + response.days[0].temp + " " + response.days[1].temp + " " + response.days[2].temp);
-    console.log("the wind gust over the next few days is:" + response.days[0].windgust + " " + response.days[1].windgust + " " + response.days[2].windgust);
-    freq = response.days[0].temp * 2; console.log(freq / 2); 
-    oscillator.frequency.setValueAtTime(freq, audioCtx.currentTime); 
-    audioCtx.resume();
-    infoField.innerHTML = "the weather in " + locationField.value + " is " + response.days[0].temp
-    if (response.days[0].temp < 60) {
-      infoField.innerHTML += " ❄️"
-    }
-    else {
-      infoField.innerHTML += " ☀️"
-    }
+
+    let temp = response.days[0].temp;
+    let humidity = response.days[0].humidity;
+
+    infoField.innerHTML = "the weather in " + locationField.value + " is " + temp + " with " + humidity + " humidity"
+
+    drawLandscape(temp, humidity);
   })
-	.catch(err => console.error(err));
+	.catch(err => 
+    console.error(err));
+    infoField.innerHTML = "Error fetching data. Check your location name.";
 }
 
-function stop(){
-  audioCtx.suspend();
+function drawLandscape(temp, humidiity){
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+  let hue = 240 - (temp * 2);
+  ctx.fillStyle = 'hsl(' + hue + ', 100%, 60%)'
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  let mountainHeight = humidity * 3;
+  ctx.fillStyle = 'darkblue';
+  ctx.beginPath();
+  ctx.moveTo(0, canvas.height);
+  ctx.lineTo(canvas.width/2, canvas.height - mountainHeight);
+  ctx.lineTo(canvas.width, canvas.height);
+  ctx.fill();
 }
-
-
-
-
 
 
